658d4fcfa67154e2303ef4457efaa35e1f169cde
Caryl Marie <2161397@slu.edu.ph>
2018-04-22 22:26:59 +0800

    fixed files

---
+++ MIDTERM FILES/9346A-G2/9346A-G2/9346A-G2/website/node.js
@@ -0,0 +1,52 @@
+function changeAlignmentButtonPrelims(){
+	
+
+	var text = document.getElementById("Prelims").style.transition="all .5s ease";
+	var text = document.getElementById("Prelims").style.transform = "translateY(10px)";	
+	var text = document.getElementById("Prelims-Button").style["boxShadow"] = "0 20px 30px -10px";
+		
+}
+
+	
+	
+function backToNormalStyleButtonPrelims(){
+	
+	var text = document.getElementById("Prelims-Button").style["boxShadow"]="0 0 0px ";	
+	var text = document.getElementById("Prelims").style.transform = "translateY(0px)"
+	
+}
+
+	function changeAlignmentButtonMidterms(){
+	
+	var text = document.getElementById("Midterms").style.transition="all .5s ease";
+	var text = document.getElementById("Midterms").style.transform = "translateY(10px)";	
+	var text = document.getElementById("Midterms-Button").style["boxShadow"] = "0 20px 30px -10px";
+	
+}
+
+	
+	
+function backToNormalStyleButtonMidterms(){
+		
+	var text = document.getElementById("Midterms-Button").style["boxShadow"]="0 0 0px ";	
+	var text = document.getElementById("Midterms").style.transform = "translateY(0px)"
+	
+}
+
+	function changeAlignmentButtonFinals(){
+	
+	var text = document.getElementById("Finals").style.transition="all .5s ease";
+	var text = document.getElementById("Finals").style.transform = "translateY(10px)";	
+	var text = document.getElementById("Finals-Button").style["boxShadow"] = "0 20px 30px -10px";	
+}
+
+	
+	
+function backToNormalStyleButtonFinals(){
+	
+	var text = document.getElementById("Finals-Button").style["boxShadow"]="0 0 0px ";	
+	var text = document.getElementById("Finals").style.transform = "translateY(0px)";
+	
+}
+
+	
