38a4c6d8b9dbb00e94667af563e56a93e7a219cf
jamspicaa <34190100+jamspicaa@users.noreply.github.com>
2018-04-27 05:06:05 +0800

    PHP file

---
+++ FINALS FILES/FINALS HTML FOLDER/Quiz1.php
@@ -0,0 +1,67 @@
+
+<head>
+    <title>Finals</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Activity.css">
+    <link rel="shortcut icon" href="images/globe.png" type="image/png">
+</head>
+<body>
+    <div class="Header-Container">
+        <nav>
+            <div class="logo">Web Technology</div>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="Notes.html">Notes</a></li>
+                <li><a href="Activity.html">Activities</a></li>
+                <li><a href="About.html">About</a></li>
+            </ul>
+        </nav> 
+    </div>
+    <div class="Quiz-Container">
+        <h1>Test your skills JavaScript</h1>
+
+        <form action="grade.php" method="post" id="quiz">
+            <ol>
+               <li>
+
+                        <h3>Inside which HTML element do we put the JavaScript?</h3>
+
+                        <div>
+                            <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                            <label for="question-1-answers-A">A) script </label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                            <label for="question-1-answers-B">B) javascript</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                            <label for="question-1-answers-C">C) js</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                            <label for="question-1-answers-D">D) scripting</label>
+                        </div>
+                    </li>
+             </ol>
+
+                <input type="submit" value="Submit Quiz" />
+            
+    </form>
+        
+    </div> 
+</body>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
