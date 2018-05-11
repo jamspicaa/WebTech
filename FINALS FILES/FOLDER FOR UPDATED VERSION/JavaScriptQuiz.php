81a3cae4e2f84fe4f4ab5c7e4df2c35861be9e00
jamspicaa <34190100+jamspicaa@users.noreply.github.com>
2018-05-12 03:31:02 +0800

    Additional Php Files

---
+++ FINALS FILES/FOLDER FOR UPDATED VERSION/JavaScriptQuiz.php
@@ -0,0 +1,220 @@
+
+<head>
+    <title>Finals</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Activity.css">
+    <link rel="shortcut icon" href="images/globe.png" type="image/png">
+</head>
+<body>
+    <div class="Header-Container">
+       
+    </div>
+    <div class="Quiz-Container">
+        <h1>Test your skills JavaScript</h1>
+
+        <form action="grade.php" method="post" id="quiz">
+            <ol>
+                <li>
+                   <h3>Inside which HTML element do we put the JavaScript?</h3>
+                
+                   <div>
+                       <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) script </label>
+                   </div>
+
+                   <div>
+                       <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                       <label for="question-1-answers-B">B) javascript</label>
+                   </div>
+
+                   <div>
+                       <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                       <label for="question-1-answers-C">C) js</label>
+                   </div>
+
+                   <div>
+                       <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                       <label for="question-1-answers-D">D) scripting</label>
+                   </div>
+                </li>
+				
+				<li>
+    				<h3>Where is the correct place to insert a JavaScript?</h3>
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A)The body section</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B)The head section</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C)Both the head section and the body section are correct</label>
+                        </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">"D)The footer section"</label>
+                    </div>	
+                </li>
+                
+				<li>
+                    <h3>What is the correct syntax for referring to an external script called "xxx.js"?</h3>    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) script name="xxx.js" </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B)  script src="xxx.js" </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) script href="xxx.js"</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) script hrefjs="xxx.js"</label>
+                    </div>
+				</li>
+				
+				<li>
+                    <h3>The external JavaScript file must contain the script tag.</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) True </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) False</label>
+                    </div>
+
+				</li>
+				
+				<li>
+				    <h3>How do you write "Hello World" in an alert box?</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) msgBox("Hello World"); </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) alert("Hello World");</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) alertBox("Hello World");</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) msg("Hello World"); </label>
+                    </div>
+				</li>
+				
+				<li>
+				    <h3>How do you create a function in JavaScript?</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) function myFunction() </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) function:myFunction()</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) function = myFunction()</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) function(myFunction())</label>
+                    </div>
+				
+				</li>
+				
+				<li>
+				    <h3> How do you call a function named "myFunction"?</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) call function myFunction() </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) call myFunction()</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) call myFunction() function</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) myFunction()</label>
+                    </div>
+				
+				</li>
+				
+				<li>
+				    <h3>How to write an IF statement in JavaScript?</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) if i = 5 </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) if (i == 5)</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C)  if i == 5 then</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D)  if i = 5 then</label>
+                    </div>
+				
+				</li>
+				
+             </ol>
+
+                <input type="submit" value="Submit Quiz" />
+            
+    </form>
+        
+    </div> 
+</body>
+                        
+                                
+                        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
